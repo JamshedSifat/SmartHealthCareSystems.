63cd51d858bd270196555638b823b9a225187c54
Sanjid Shahriar Sajin <sanjidshahriar07@gmail.com>
2026-04-10 23:13:07 +0600

    Add Doctor model with relevant fields

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Doctor(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=255)
+    specialty = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+    cost = models.IntegerField()
+    available_spots = models.PositiveIntegerField()
+    next_available_appointment_date = models.DateField(null=True, blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+      
