533878945247a5b44a6beca0371746f9f92fbfd6
Sanjid Shahriar Sajin <sanjidshahriar07@gmail.com>
2026-04-10 23:14:39 +0600

    Update models.py

--- appointments/models.py
+++ appointments/models.py
@@ -11,4 +11,22 @@ class Doctor(models.Model):
 
     def __str__(self):
         return self.name
-      
+class DoctorTimeSlot(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.doctor.name} ({self.start_time} - {self.end_time})"
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    doctor_time_slot = models.ForeignKey(DoctorTimeSlot, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000)
+    appointment_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    serial_number = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
