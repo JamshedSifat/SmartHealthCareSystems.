861a918c96b0564c0a36192891679415951f0b93
MD. JAMSHED ALAM SEFAT <mdjamshedsifat@gmail.com>
2026-04-19 19:40:20 +0600

    Create UserProfile model for user details
    Added UserProfile model with user details.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    address = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=15)
+    GENDER = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER)
+
+    def __str__(self):
+        full_name = f"{self.user.first_name} {self.user.last_name}".strip()
+        return full_name or self.user.username
