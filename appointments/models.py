692e5908b46edeecb21b199e5b2ba3e1423b6f79
shanjida <23101036@uap-bd.edu>
2026-04-20 01:58:46 +0600

    Update models.py

--- appointments/models.py
+++ appointments/models.py
@@ -30,3 +30,24 @@ class Appointment(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Hospital(models.Model):
+    hospital_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    capacity = models.IntegerField()
+    blood_samples = models.ManyToManyField('Blood', related_name='hospitals')
+
+    def __str__(self):
+        return self.hospital_name
+
+class Blood(models.Model):
+    BLOOD_GROUP_CHOICES = (
+        ('A+', 'A+'), ('B+', 'B+'), ('AB+', 'AB+'), ('O+', 'O+'),
+        ('A-', 'A-'), ('B-', 'B-'), ('AB-', 'AB-'), ('O-', 'O-'),
+    )
+    blood_group = models.CharField(max_length=20, choices=BLOOD_GROUP_CHOICES)
+    quantity = models.PositiveIntegerField()
+    expiry_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.blood_group}"
